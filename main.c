cd70343534c5f429c20ea225fbd93b5ef6d8b3f3
capricey <zhelannikovandrey@gmail.com>
2022-05-13 14:34:42 +0300

    sin

--- main.c
+++ main.c
@@ -22,12 +22,29 @@ void run_testcase(Suite *testcase) {
 }
 
 void run_tests(void) {
-    Suite *list_cases[] = {suite_s21_abs(),   suite_s21_acos(), suite_s21_asin(), suite_s21_atan(),
-                           suite_s21_ceil(),  suite_s21_cos(),  suite_s21_exp(),  suite_s21_fabs(),
-                           suite_s21_floor(), suite_s21_fmod(), suite_s21_log(),  suite_s21_pow(),
-                           suite_s21_sin(),   suite_s21_sqrt(), suite_s21_tan(),  NULL};
+    Suite *list_cases[] = {// suite_s21_abs(),  // suite_s21_acos(), suite_s21_asin(), suite_s21_atan(),
+                           // suite_s21_ceil(),  suite_s21_cos(),  suite_s21_exp(),  suite_s21_fabs(),
+                           // suite_s21_floor(), suite_s21_fmod(), suite_s21_log(),  suite_s21_pow(),
+                           suite_s21_sin(),  //   suite_s21_sqrt(), suite_s21_tan(),
+                           NULL};
 
     for (Suite **current_testcase = list_cases; *current_testcase != NULL; current_testcase++) {
         run_testcase(*current_testcase);
     }
+}
+
+double RandomReal(double low, double high) {
+    double d;
+
+    d = (double)rand() / ((double)RAND_MAX + 1);
+    return (low + d * (high - low));
+}
+
+int RandomInteger(int low, int high) {
+    int k;
+    double d;
+
+    d = (double)rand() / ((double)RAND_MAX + 1);
+    k = (int)(d * (high - low + 1));
+    return (low + k);
 }
--- s21_sin.c
+++ s21_sin.c
@@ -1,5 +1,17 @@
 #include "s21_math.h"
 
 long double s21_sin(double x) {
-    //
+    long double tmp = (long double)x;
+    // fmod(tmp, M_PI / 180.0);
+    //  printf("%Lf\n", 180.0 / M_PI);
+    //  fmod(x, (180.0 / M_PI));  // s21_fmod (!)
+    long double res = 0;
+    for (int i = 0; i < 5 && (2 * i + 1) <= 20; i++) {
+        // printf("%Lf\n", pow(x, 2 * i + 1));
+        // printf("%Lf\n", pow(-1, i));
+
+        res += pow(-1, i) * (pow(x, 2 * i + 1) / (long double)factorials[2 * i + 1]);  // s21_pow (!)
+        // printf("RES%d: %Lf\n", i, res);
+    }
+    return res;
 }
