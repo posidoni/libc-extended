c5e99069303fed82f9a764c5106352d06103ace5
Caprice Ytrams <capricey@ga-s6.msk.21-school.ru>
2022-05-13 23:12:15 +0300

    sin + cos done

--- s21_tan.c
+++ s21_tan.c
@@ -1,5 +1,23 @@
 #include "s21_math.h"
 
 long double s21_tan(double x) {
-    //
+    long double res = 0.0;
+    long double n = (long double)x;
+
+    long double m = 1;
+    if (n < 0) {
+        n = -n;
+        m = -1;
+    }
+
+    n = fmodl(n, (long double)(M_PI));
+
+    if (n > M_PI / 2.0) {
+        n = M_PI - n;
+        m = -1;
+    }
+
+    res = n + (powl(n, 3.0) / 3.0) + (2.0 * powl(n, 5.0) / 15.0) + (17.0 * powl(n, 7.0) / 315.0);
+
+    return m * res;
 }
--- tests/test_s21_cos.c
+++ tests/test_s21_cos.c
@@ -1,10 +1,17 @@
 #include "s21_math_test.h"
 
+START_TEST(cos_test_1) {
+    double x = RandomReal(-1e10, 1e10);
+    ck_assert_double_eq_tol(s21_cos(x), cos(x), 1e-06);
+}
+END_TEST
+
 Suite *suite_s21_cos(void) {
-    Suite *s = suite_create("suite_s21_add");
-    TCase *tc = tcase_create("s21_add_tc");
+    Suite *s = suite_create("suite_s21_cos");
+    TCase *tc = tcase_create("s21_cos_tc");
 
     // tcase_add_test(tc, );
+    tcase_add_loop_test(tc, cos_test_1, 0, 1000);
 
     suite_add_tcase(s, tc);
     return s;
