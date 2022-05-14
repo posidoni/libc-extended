f061c831ec86ac0419c9c19a9f0921f124fc454b
capricey <zhelannikovandrey@gmail.com>
2022-05-14 13:16:36 +0300

    tan progress

--- tests/test_s21_tan.c
+++ tests/test_s21_tan.c
@@ -3,11 +3,9 @@
 START_TEST(tan_test_1) {
     double x = RandomReal(-1e10, 1e10);
     // ck_assert_double_eq_tol(s21_tan(x), tan(x), 1e-06);
-    long double a = s21_tan(x);
-    long double b = tan(x);
-    printf("A: %6Lf\n", a);
-    printf("B: %6Lf\n", b);
-    ck_assert_ldouble_eq_tol(s21_tan(x), (long double)tan(x), 0.000001);
+    // printf("A: %6Lf\n", a);
+    // printf("B: %6Lf\n", b);
+    ck_assert_ldouble_eq_tol(s21_tan(x), (long double)tan(x), 1e-06);
 }
 END_TEST
 
@@ -15,7 +13,7 @@ Suite *suite_s21_tan(void) {
     Suite *s = suite_create("suite_s21_tan");
     TCase *tc = tcase_create("s21_tan_tc");
 
-    tcase_add_loop_test(tc, tan_test_1, 0, 1000);
+    tcase_add_loop_test(tc, tan_test_1, 0, 10);
 
     suite_add_tcase(s, tc);
     return s;
