bcfa4280e392bc8c4a41b7927bc5ff09e3d7a5c4
nanadaug <nanadaug@student.21-school.ru>
2022-05-15 18:30:30 +0000

    fix test pow

--- tests/test_s21_pow.c
+++ tests/test_s21_pow.c
@@ -4,11 +4,9 @@
 START_TEST(basic_test) {
     long double base;
     long double exp;
-    for (int i = -666; i < 666; i++) {
-        base = ((double)rand()/(double)RAND_MAX);
-        exp = ((double)rand()/(double)RAND_MAX) * (rand() % 11 ? 1 : -1);
-        ck_assert_double_eq_tol(s21_pow(base, exp), pow(base, exp), EPS);
-    }
+    base = RandomReal(0, 100);
+    exp = RandomReal(-4, 4) * (rand() % 11 ? 1 : -1);
+    ck_assert_double_eq_tol(s21_pow(base, exp), pow(base, exp), EPS);
 } END_TEST
 
 START_TEST(base_niga) {
@@ -27,6 +25,7 @@ Suite *suite_s21_pow(void) {
     Suite *s = suite_create("suite_s21_pow");
     TCase *tc = tcase_create("s21_add_tc");
 
+    tcase_add_loop_test(tc, basic_test, -666, 666);
      tcase_add_test(tc, basic_test);
      tcase_add_test(tc, base_niga);
      tcase_add_test(tc, all_niga);
