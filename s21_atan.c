ee37edd8d6d15b7b97d2acf8964bfb509e92fab1
Posidonius Kathyrn <posidoni@su-g5.msk.21-school.ru>
2022-05-15 20:40:11 +0300

    Developed atan

--- s21_atan.c
+++ s21_atan.c
@@ -2,18 +2,18 @@
 
 long double s21_atan(double x) {
     long double res = 0.0;
-    long double sign = -1.0;
-    long double n = (long double)x;
+    /* Sqrt of square looks a bit reduntant, but the formula was like that (?) */
+    long double magic = (M_PI * sqrt(x * x)) / (2 * x);
+    long double n = x;
+    int modulo_x = (fabs(x) < 0);
 
-    long double magic = 3;
-    long double exp = 3;
-
-    for (int i = 0; exp < 16; i++) {
-        res += sign * (powl(n, exp) / magic);
-        exp += 2;
-        magic += 2;
-        sign = -sign;
+    for (int i = 0; i < 20; i++) {
+        if (modulo_x) {
+            res += (pow(-1, i) * pow(x, 1 + 2 * i)) / (1 + 2 * i);
+        } else {
+            res += (pow(-1, i) * pow(x, (-1 - 2 * i))) / (1 + 2 * i);
+        }
     }
 
-    return res;
+    return (modulo_x < 0) ? res : magic - res;
 }
--- tests/test_s21_atan.c
+++ tests/test_s21_atan.c
@@ -2,6 +2,8 @@
 
 START_TEST(atan_test_1) {
     double x = RandomReal(-1e10, 1e10);
+
+    ck_assert_ldouble_eq_tol(s21_atan(x), atan(x), 1e-6);
 }
 END_TEST
 
@@ -10,7 +12,7 @@ Suite *suite_s21_atan(void) {
     TCase *tc = tcase_create("s21_add_tc");
 
     // tcase_add_test(tc, );
-    tcase_add_loop_test(tc, atan_test_1, 0, 15);
+    tcase_add_loop_test(tc, atan_test_1, 0, 10000);
 
     suite_add_tcase(s, tc);
     return s;
