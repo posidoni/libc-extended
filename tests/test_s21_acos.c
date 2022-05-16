8d2844b803af109b97bba9cb2aac61caee483939
Posidonius Kathyrn <posidoni@su-g5.msk.21-school.ru>
2022-05-16 16:28:32 +0300

    Fully implemented and tested acos

--- tests/test_s21_acos.c
+++ tests/test_s21_acos.c
@@ -1,10 +1,58 @@
 #include "s21_math_test.h"
 
+START_TEST(acos_test_1_rand) {
+    double x = RandomReal(-1e10, 1e10);
+
+    if (x == 0) {
+        ck_assert_ldouble_eq_tol(s21_acos(x), 0.0, 1e-6);
+        ck_assert_ldouble_eq_tol(acos(x), 0.0, 1e-6);
+    } else if (fabsl(x) > 1.0 || isnan(x)) {
+        /* Errno should be EDOM (error domain error) */
+        ck_assert_ldouble_nan(s21_acos(x));
+        ck_assert_int_eq(errno, EDOM);
+        ck_assert_ldouble_nan(acos(x));
+        ck_assert_int_eq(errno, EDOM);
+    } else {
+        ck_assert_ldouble_eq_tol(s21_acos(x), acos(x), 1e-6);
+    }
+}
+END_TEST
+
+START_TEST(acos_test_2_inf) {
+    ck_assert_ldouble_nan(s21_acos(NAN));
+    ck_assert_int_eq(errno, EDOM);
+    ck_assert_ldouble_nan(acos(NAN));
+    ck_assert_int_eq(errno, EDOM);
+}
+END_TEST
+
+START_TEST(acos_test_3_nan) {
+    ck_assert_ldouble_nan(s21_acos(NAN));
+    ck_assert_ldouble_nan(acos(NAN));
+}
+END_TEST
+
+START_TEST(acos_test_4_one) {
+    ck_assert_ldouble_eq_tol(s21_acos(1.0), acos(1.0), 1e-6);
+    ck_assert_ldouble_eq_tol(s21_acos(-1.0), acos(-1.0), 1e-6);
+}
+END_TEST
+
+START_TEST(acos_test_5_zero) {
+    ck_assert_ldouble_eq_tol(s21_acos(0.0), acos(0.0), 1e-6);
+    ck_assert_ldouble_eq_tol(s21_acos(-0.0), acos(-0.0), 1e-6);
+}
+END_TEST
+
 Suite *suite_s21_acos(void) {
-    Suite *s = suite_create("suite_s21_add");
+    Suite *s = suite_create("suite_s21_acos");
     TCase *tc = tcase_create("s21_add_tc");
 
-    // tcase_add_test(tc, );
+    tcase_add_loop_test(tc, acos_test_1_rand, 0, 10000);
+    tcase_add_test(tc, acos_test_2_inf);
+    tcase_add_test(tc, acos_test_3_nan);
+    tcase_add_test(tc, acos_test_4_one);
+    tcase_add_test(tc, acos_test_5_zero);
 
     suite_add_tcase(s, tc);
     return s;
