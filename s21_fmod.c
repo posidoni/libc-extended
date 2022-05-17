5acc86dca16b788a11a1c95eee13bc372ff16279
Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>
2022-05-17 17:49:21 +0300

    minor fixes

--- s21_fmod.c
+++ s21_fmod.c
@@ -2,12 +2,15 @@
 
 long double s21_fmod(double x, double y) {
     // returns a NaN for x infinite or y zero.
-    if (is_nan(x) || is_nan(y) || (is_inf(x) && is_inf(y)) || (is_inf(x) || y == 0)) return S21_NAN;
-
+    if (is_nan(x) || is_nan(y) || is_inf(x) || fabs(y) < EPS ||
+        (is_inf(x) && is_inf(y)))
+        return S21_NAN;
     // fmod(+-0, y) returns +-0 if y is neither 0 nor NaN
-    if (x == 0 && !is_nan(y) && y != 0) return 0;
+    if (fabs(x) < EPS)
+        return 0;
     // fmod(x, +-infinity) returns x for x not infinite.
-    if (is_inf(y) && !is_inf(x)) return x;
+    if (is_inf(y))
+        return x;
 
     long double lx = x;
     long double ly = y;
