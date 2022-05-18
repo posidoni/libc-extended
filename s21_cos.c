8c03f54723d25cb31c034aab448eb3a0457d3caa
Caprice Ytrams <capricey@ga-s6.msk.21-school.ru>
2022-05-18 16:26:41 +0300

    style fix

--- s21_cos.c
+++ s21_cos.c
@@ -1,29 +1,26 @@
 #include "s21_math.h"
 
 long double s21_cos(double x) {
-    if (x == S21_NAN || x == S21_INF || x == -S21_INF) return S21_NAN;
+    if (is_nan(x) || !is_finite(x)) return S21_NAN;
 
-    long double t, s;
-    int m = 1;
+    int sign = 1;
     x = s21_fmod(x, 2 * S21_M_PI);
 
     if (x > (S21_M_PI / 2.0) && x <= S21_M_PI) {
         x = S21_M_PI - x;
-        m = -m;
+        sign = -sign;
     } else if (x > S21_M_PI && x <= S21_M_PI * 3.0 / 2.0) {
-        x = (x - S21_M_PI);
-        m = -m;
+        x -= S21_M_PI;
+        sign = -sign;
     } else if (x > (S21_M_PI * 3.0) / 2.0 && x <= 2.0 * S21_M_PI) {
         x = 2 * S21_M_PI - x;
     }
 
-    int p = 0;
-    s = 1.0;
-    t = 1.0;
-    while (s21_fabs(t / s) > 1e-100) {
-        p++;
-        t = (-t * x * x) / ((2 * p - 1) * (2 * p));
-        s += t;
+    long double sum = 1.0;
+    long double tailor = 1.0;
+    for (int p = 1; s21_fabs(tailor / sum) > 1e-100; p++) {
+        tailor = (-tailor * x * x) / ((2.0 * p - 1) * (2.0 * p));
+        sum += tailor;
     }
-    return s * m;
+    return sum * sign;
 }
--- s21_sin.c
+++ s21_sin.c
@@ -30,29 +30,26 @@
 // }
 
 long double s21_sin(double x) {
-    if (x == S21_NAN || x == S21_INF || x == -S21_INF) return S21_NAN;
+    if (is_nan(x) || !is_finite(x)) return S21_NAN;
 
-    int m = 1;
+    int sign = 1;
     x = s21_fmod(x, 2 * S21_M_PI);
 
     if (x > (S21_M_PI / 2.0) && x <= S21_M_PI) {
         x = S21_M_PI - x;
     } else if (x > S21_M_PI && x <= S21_M_PI * 3.0 / 2.0) {
         x = (x - S21_M_PI);
-        m = -m;
+        sign = -sign;
     } else if (x > (S21_M_PI * 3.0) / 2.0 && x <= 2.0 * S21_M_PI) {
         x = 2 * S21_M_PI - x;
-        m = -m;
+        sign = -sign;
     }
 
-    long double t, s;
-    s = (long double)x;
-    t = (long double)x;
-    int p = 0;
-    while (s21_fabs(t / s) > 1e-100) {
-        p++;
-        t = (-t * x * x) / ((2.0 * p + 1) * (2.0 * p));
-        s += t;
+    long double sum = (long double)x;
+    long double tailor = (long double)x;
+    for (int p = 1; s21_fabs(tailor / sum) > 1e-100; p++) {
+        tailor = (-tailor * x * x) / ((2.0 * p + 1) * (2.0 * p));
+        sum += tailor;
     }
-    return s * m;
+    return sum * sign;
 }
