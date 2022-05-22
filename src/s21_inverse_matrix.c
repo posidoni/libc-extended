714bedd3cd52e0f17c3fe2309f8e24ec7fa08b56
MikhailKuznetsov1 <posidoni@LENOVO-DESKTOP.localdomain>
2022-05-22 22:45:39 +0300

    Add notes to inverse method

--- src/s21_inverse_matrix.c
+++ src/s21_inverse_matrix.c
@@ -1,4 +1,9 @@
 #include "s21_matrix.h"
+// https://www.geeksforgeeks.org/adjoint-inverse-matrix/
+
+// find determinant (it must be non zero) 
+// find adjoint -> divide det by adjoint  
+
 
 // int s21_inverse_matrix(matrix_t *A) {
 // }
