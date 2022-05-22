c18977b5ff734dddb022e54af75fb963b87eb5a6
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-05-22 18:57:08 +0300

    Add matr_mult, transpose

--- src/tests/test_s21_transpose.c
+++ src/tests/test_s21_transpose.c
@@ -15,7 +15,8 @@ START_TEST(transpose_matrix) {
             m.matrix[i][j] = rand_val;
             check.matrix[j][i] = rand_val;
         }
-    
+    }
+
     matrix_t res = {0};
     s21_create_matrix(cols, rows, &res);
     ck_assert_int_eq(s21_transpose(&m, &res), OK);
@@ -27,12 +28,13 @@ START_TEST(transpose_matrix) {
 }
 END_TEST
 
-Suite *suite_s21_transpose_matrix(void) {
-    Suite *s = suite_create("suite_s21_transpose_matrix");
-    TCase *tc = tcase_create("s21_transpose_matrix");
+Suite *suite_s21_transpose(void) {
+    Suite *s = suite_create("suite_s21_transpose");
+    TCase *tc = tcase_create("s21_transpose");
 
     tcase_add_loop_test(tc, transpose_matrix, 0, 1000);
 
     suite_add_tcase(s, tc);
+
     return s;
 }
