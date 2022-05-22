5d58dd7e88edaba03f1e3bd8d4fc521058313aad
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-05-22 14:02:52 +0300

    removed tmp

--- src/tests/test_s21_matrix.h
+++ src/tests/test_s21_matrix.h
@@ -1,10 +1,11 @@
 #ifndef TEST
 #define TEST
 
-#include "../s21_matrix.h"
 #include <check.h>
 #include <stdlib.h>
 #include <time.h>
+
+#include "../s21_matrix.h"
 
 Suite *suite_s21_eq_matrix(void);
 Suite *suite_s21_create_matrix(void);
@@ -13,6 +14,7 @@ Suite *suite_s21_sub_matrix(void);
 Suite *suite_s21_mult_number_matrix(void);
 Suite *suite_s21_mult_matrix(void);
 Suite *suite_s21_transpose_matrix(void);
+
 void run_tests(void);
 void run_testcase(Suite *testcase);
 
