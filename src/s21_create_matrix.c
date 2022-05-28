dd40a256655e1e9bbea548d58684eeb7f178f24a
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-05-28 12:24:51 +0300

    📌 Restructured project dir, add readme credits

---
+++ src/s21_create_matrix.c
@@ -0,0 +1,24 @@
+#include "s21_matrix.h"
+
+int s21_create_matrix(const int rows, const int columns, matrix_t *result) {
+    int res = INCORRECT_MATRIX;
+
+    if (rows > 0 && columns > 0) {
+        result->columns = columns;
+        result->rows = rows;
+        result->matrix = calloc(rows, sizeof(double *));
+        res = MALLOC_FAILED;
+
+        if (result->matrix) {
+            for (int i = 0; i < rows; i++) {
+                result->matrix[i] = calloc(columns, sizeof(double));
+                if (!result->matrix[i])
+                    return MALLOC_FAILED;
+                // (?) memory cleanup after failed malloc (?)
+            }
+            res = OK;
+        }
+    }
+
+    return res;
+}
