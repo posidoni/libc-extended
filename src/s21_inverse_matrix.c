306a39adb4dbe42fd2896080e4e23648391e9d91
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-29 10:07:13 +0300

    fix not mallocing matr in inverse

--- src/s21_inverse_matrix.c
+++ src/s21_inverse_matrix.c
@@ -10,26 +10,29 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
         return CALC_ERROR;
 
     double d = 0.0;
+    int code = s21_create_matrix(A->rows, A->columns, result);
 
-    s21_determinant(A, &d);
+    if (code == OK) {
+        s21_determinant(A, &d);
 
-    if (fabs(d) < 1e-6)
-        return CALC_ERROR;
+        if (fabs(d) < 1e-6)
+            return CALC_ERROR;
 
-    matrix_t adj1 = {0};
-    s21_create_matrix(A->rows, A->columns, &adj1);
-    s21_calc_complements(A, &adj1);
-    matrix_t adj = {0};
-    s21_create_matrix(A->columns, A->rows, &adj);
-    s21_transpose(&adj1, &adj);
+        matrix_t adj1 = {0};
+        s21_create_matrix(A->rows, A->columns, &adj1);
+        s21_calc_complements(A, &adj1);
+        matrix_t adj = {0};
+        s21_create_matrix(A->columns, A->rows, &adj);
+        s21_transpose(&adj1, &adj);
 
-    // adjoint
-    int size = A->rows;
+        int size = A->rows;
 
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            result->matrix[i][j] = adj.matrix[i][j] / d;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                result->matrix[i][j] = adj.matrix[i][j] / d;
 
-    s21_remove_matrix(&adj);
-    return OK;
+        s21_remove_matrix(&adj);
+    }
+
+    return (code);
 }
